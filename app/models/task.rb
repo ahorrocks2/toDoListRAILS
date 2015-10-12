35cc79c51983dcfb3ca11e467b3e7c9a1fdde91d
Alyssa Horrocks & Justin Scott <student@epicodus.com>
2015-10-12 11:37:46 -0700

    Add ability to mark task as complete and show in separate list.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,9 @@
 class Task < ActiveRecord::Base
   belongs_to :list
-
   validates :description, :presence => true
+
+  scope(:not_done, -> do
+    where({:done => false})
+  end)
 
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,4 +3,14 @@ require 'rails_helper'
 describe Task do
   it { should validate_presence_of :description }
   it { should belong_to :list }
+
+  describe(".not_done") do
+    it("returns not done tasks") do
+      task1 = Task.create({:description => "task1", :done=> false})
+      task2 = Task.create({:description => "task2", :done=> false})
+      not_done_tasks = [task1, task2]
+      done_task = Task.create({:description => "im done", :done=> true})
+      expect(Task.not_done()).to(eq(not_done_tasks))
+    end
+  end
 end
